405cfca4e39cd51019943f3a105970d8b87e3de6
breakpack <198799879a@gmail.com>
2024-09-09 17:46:39 +0900

    Make Base

---
+++ aegislenz-font/src/component/content/content.js
@@ -0,0 +1,7 @@
+import * as S from "./Content_style";
+
+function Content() {
+  return <S.Wrapper></S.Wrapper>;
+}
+
+export default Content;
---
+++ aegislenz-font/src/component/sideBar/sideBar.js
@@ -0,0 +1,22 @@
+import * as S from "./sideBar_style";
+import { Link } from "react-router-dom";
+
+function SideBar() {
+  return (
+    <S.Wrapper>
+      <S.IconWrapper>
+        <Link to="/">
+          <S.Icon src="/icon/Frame.png"></S.Icon>
+        </Link>
+        <Link to="/AI">
+          <S.Icon src="/icon/AI_Icon.png"></S.Icon>
+        </Link>
+        <Link to="/Network">
+          <S.Icon src="/icon/Network_Icon.png"></S.Icon>
+        </Link>
+      </S.IconWrapper>
+    </S.Wrapper>
+  );
+}
+
+export default SideBar;
