98f9e4903c5fa9035eaeb94c73241857f4e6858e
breakpack <198799879a@gmail.com>
2024-10-28 22:25:48 +0900

    Feat: IAM 상세 Top Title 제작 완료 (#43)

--- src/component/innerIam/InnerIam.js
+++ src/component/innerIam/InnerIam.js
@@ -1,6 +1,21 @@
 import * as S from "./InnerIam_style";
 
-let Data = ["User1", "AWS", "2024-10-15", "SE", 1];
+let Data = {
+  UserName: "Wonje_Cha",
+  UserId: "AIDA2MNVLVQT5TARLHOZP",
+  CreateDate: "2024-10-01T04:11:48+00:00",
+  UserPolicies: [],
+  AttachedPolicies: ["AmazonEC2FullAccess", "AdministratorAccess"],
+  Groups: [],
+  AccessKeysLastUsed: [
+    {
+      AccessKeyId: "AKIA2MNVLVQT53CDZV4V",
+      Status: "Active",
+      LastUsedDate: "2024-10-26T09:24:00+00:00",
+    },
+  ],
+  LastUpdated: "2024-10-26T20:11:27.101264",
+};
 
 const Inneriam = () => {
   return (
@@ -8,12 +23,14 @@ const Inneriam = () => {
       <S.InnerTop>
         <S.InnerTitleArea>
           <S.InnerTitleIcon src="/icon/AI_Icon.png" />
-          <S.InnerTitle service={Data[3]}>{Data[0]}</S.InnerTitle>
-          <S.InnerTopInfo>
-            <S.InfoLink to="#">{Data[1]}</S.InfoLink>
-            <S.InfoLink to="#">{Data[2]}</S.InfoLink>
-          </S.InnerTopInfo>
+          <S.InnerTitle UserId={"UserId: " + Data.UserId}>
+            {Data.UserName}
+          </S.InnerTitle>
         </S.InnerTitleArea>
+        <S.InnerTopInfo>
+          <S.InfoLink to="#">{"AWS"}</S.InfoLink>
+          <S.InfoLink to="#">{Data.CreateDate}</S.InfoLink>
+        </S.InnerTopInfo>
       </S.InnerTop>
       <S.InnerCategorWrapper></S.InnerCategorWrapper>
     </S.Wrapper>
--- src/component/innerIam/InnerIam_style.js
+++ src/component/innerIam/InnerIam_style.js
@@ -17,7 +17,7 @@ export const Wrapper = styled.div`
 export const InnerTop = styled.div`
   position: relative;
   width: 90%;
-  height: 16%;
+  height: 18%;
   border-bottom: solid 1px #aaa;
 `;
 export const InnerTitleArea = styled.div`
@@ -25,38 +25,41 @@ export const InnerTitleArea = styled.div`
   align-items: center;
   justify-content: flex-start;
   flex-direction: row;
+  position: relative;
 
-  width: 40%;
+  width: 70%;
   height: 80%;
   margin-top: 3%;
 `;
 export const InnerTitle = styled.h1`
-  position: relative;
-  margin: 0 0 7% 3%;
+  margin: 0 0 4% 3%;
   padding: 0;
 
   &::after {
-    content: "${(props) => props.service}";
+    content: "${(props) => props.UserId}";
     position: absolute;
     font-size: 50%;
-    left: 2%;
-    bottom: -45%;
+    left: 12%;
+    bottom: 30%;
     opacity: 0.5;
   }
 `;
 export const InnerTopInfo = styled.div`
   position: absolute;
+
+  display: flex;
+  flex-direction: row;
+  width: 100%;
   bottom: 0;
   left: 7%;
   padding: 1%;
-  width: 20%;
 `;
 export const InfoLink = styled(Link)`
-  margin-right: 7%;
+  margin-right: 2%;
   text-decoration: none;
   color: black;
   opacity: 0.5;
-
+  border-top: solid 1px #333;
   transition: all ease 0.3s;
   &:hover {
     opacity: 1;
